7f9bd01442c855050e1611dc78efbbc69cc59b47
JustJokingAround <imageheart89@gmail.com>
2025-02-07 02:04:06 -0600

    added course names and instructor names to the spreadsheet

--- webscrape.py
+++ webscrape.py
@@ -31,26 +31,30 @@ def parser(matching_links):
     currenturl =str(s).split()[1]
     currenturl=currenturl.split('/')[4]
     subject = currenturl[0:len(currenturl)-2]
-    url = "https://courses.illinois.edu/schedule/2024/fall/" + subject
+    url = "https://courses.illinois.edu/schedule/2025/spring/" + subject
     r = requests.get(url)
     soup = BeautifulSoup(r.text, "html.parser")
-    pattern = re.compile(r'schedule/2024/fall/' + subject + '/[^/]+')
+    pattern = re.compile(r'schedule/2025/spring/' + subject + '/[^/]+')
     other_links = soup.find_all('a', href=pattern)
     for s in other_links:
       currenturl =str(s).split()[1]
       currenturl = currenturl.split('/')[5]
       course_number = currenturl[0:len(currenturl)-2]
-      url = "https://courses.illinois.edu/schedule/2024/fall/" + subject + "/" + course_number
+      url = "https://courses.illinois.edu/schedule/2025/spring/" + subject + "/" + course_number
       driver.get(url)
       classes = subject + " " + course_number
       infos = driver.find_elements(By.XPATH, "//*[@id=\"section-dt\"]/tbody/tr")
+      name = driver.find_element(By.XPATH, "//*[@id=\"app-course-info\"]/div[1]")
+      coursename = name.text.splitlines()[0]
       for element in infos:
         column_list = element.text.splitlines()         
-        lecture, time, days, location = None, None, None, None
+        lecture, time, days, location, instructor = None, None, None, None, None
         for line in column_list:
           if ((len(column_list) >=3)) :
             if (column_list[2] == "Lecture"):
                 lecture = "Lecture"
+            if (column_list[2] == "Lecture-Discussion"):
+                lecture = "Lecture-Discussion"
             if (("AM" in line) | ("PM" in line)):
                 time = line
             if (line in combinations):
@@ -58,9 +62,15 @@ def parser(matching_links):
             if (len(column_list) > 6):
               location = column_list[6]
             else:
-              location = "TBD"            
-            if all([lecture, time, days, location]):
-              data = {"Classes": classes, "Type": lecture, "Time": time, "Days": days, "Location": location}
+              location = "TBD"  
+            if len(column_list) > 7:
+              instructor = column_list[7] 
+              if len(column_list) > 8:
+                 instructor = instructor + ", " + column_list[8]  
+            else:
+               instructor = "TBD"
+            if all([lecture, time, days, location, instructor]):
+              data = {"CourseName": coursename, "Classes": classes, "Type": lecture, "Time": time, "Days": days, "Location": location, "Instructor": instructor}
               df.append(data)
               break   
   df = pd.DataFrame(df)
@@ -68,24 +78,17 @@ def parser(matching_links):
   return df
   
             
-
-
-
-
-
-url = "https://courses.illinois.edu/schedule/2024/fall"
+url = "https://courses.illinois.edu/schedule/2025/spring"
 r = requests.get(url)
 
 
 soup = BeautifulSoup(r.text, "html.parser")
 
-
-pattern = re.compile(r'schedule/2024/fall/[^/]+')  # Match any character after 'spring/' except '/'
+pattern = re.compile(r'schedule/2025/spring/[^/]+')  # Match any character after 'spring/' except '/'
 subjects = soup.find_all('a', href=pattern)
-
 df2 = parser(subjects)
 print(df2)
-df2.to_excel("course_explorer.xlsx",
+df2.to_excel("lastone.xlsx",
              sheet_name='Sheet_name_1', index = False)
 
 
